de000f215133cb029325818d08085d2093dfa5ac
chromial <ljtyson@gmail.com>
2016-12-02 17:46:31 -0500

    Create provisioner.rb
    used to provision new machines for cluster

---
+++ provisioner.rb
@@ -0,0 +1,31 @@
+# provision new vms
+
+# directory structure
+
+directory '/tyson/prod'
+directory '/tyson/stag'
+directory '/tyson/app'
+
+# symbolic links
+
+# temporary install directory
+
+directory '/tmp/apps'
+
+# install Java
+
+# install python
+
+# install Oracle
+
+# prepare tnsnames
+
+# install application server
+
+# install scripts
+
+# install perl libraries
+
+# deploy AppDynamics agents
+
+# update login file
